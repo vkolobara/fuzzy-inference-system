1c7d5517da52f4d1589a936e65aee35e07170134
Vinko Kolobara <kolobara.vinko@gmail.com>
2017-08-07 15:11:55 +0200

    Added smooth membership functions, and added separate namespaces for smooth and partially linear functions. Documented each type of function and parameters where necessary.

--- fuzzy/MembershipFunctions.cpp
+++ fuzzy/MembershipFunctions.cpp
@@ -2,72 +2,199 @@
 // Created by vkolobara on 8/7/17.
 //
 
+#include <cmath>
 #include "MembershipFunctions.h"
 
-class GammaMembershipFunction : public MembershipFunction {
-private:
-    double alpha, beta;
- public:
-    GammaMembershipFunction(double alpha, double beta) :alpha{alpha}, beta{beta}{
-        // CHECK alpha < beta
-    }
+namespace LinearMembershipFunction {
 
-    ~GammaMembershipFunction(){}
-
-    double calculateValue(const double &x) {
-        if (x < alpha) {
-            return 0;
-        } else if (x >= beta) {
-            return 1;
-        } else {
-            return (x - alpha) / (beta - alpha);
+    /*
+     * Usually, Gamma membership function is used in 'large', 'high' concepts.
+     *
+     */
+    class GammaMembershipFunction : public MembershipFunction {
+    private:
+        double alpha, beta;
+    public:
+        GammaMembershipFunction(double alpha, double beta) : alpha{alpha}, beta{beta} {
+            // CHECK alpha < beta
         }
-    }
-};
+
+        ~GammaMembershipFunction() {}
+
+        double calculateValue(const double &x) {
+            if (x < alpha) {
+                return 0;
+            } else if (x >= beta) {
+                return 1;
+            } else {
+                return (x - alpha) / (beta - alpha);
+            }
+        }
+    };
 
 
-class LambdaMembershipFunction : public MembershipFunction {
-private:
-    double alpha, beta, gamma;
-public:
-    LambdaMembershipFunction(double alpha, double beta, double gamma) :alpha{alpha}, beta{beta}, gamma{gamma}{
-        // CHECK alpha < beta < gamma
-    }
-    ~LambdaMembershipFunction(){}
+    /*
+     * Lambda membership functions is used for the 'around' concept.
+     *
+     */
+    class LambdaMembershipFunction : public MembershipFunction {
+    private:
+        double alpha, beta, gamma;
+    public:
+        LambdaMembershipFunction(double alpha, double beta, double gamma) : alpha{alpha}, beta{beta}, gamma{gamma} {
+            // CHECK alpha < beta < gamma
+        }
 
-    double calculateValue(const double &x) {
-        if (x < alpha || x >= gamma) return 0;
-        else if (x < beta && x >= alpha) return (x - alpha) / (beta - alpha);
-        else return (gamma - x) / (gamma - beta);
-    }
+        ~LambdaMembershipFunction() {}
 
-};
+        double calculateValue(const double &x) {
+            if (x < alpha || x >= gamma) return 0;
+            else if (x < beta && x >= alpha) return (x - alpha) / (beta - alpha);
+            else return (gamma - x) / (gamma - beta);
+        }
 
-class LMembershipFunction : public GammaMembershipFunction {
-public:
+    };
 
-    LMembershipFunction(double alpha, double beta) : GammaMembershipFunction(alpha, beta) {}
+    /*
+     * L membership function is used as the exact opposite to the gamma function. It represents 'low' concept.
+     */
+    class LMembershipFunction : public GammaMembershipFunction {
+    public:
 
-    double calculateValue(const double &x) {
-        return 1 - GammaMembershipFunction::calculateValue(x);
-    }
-};
+        LMembershipFunction(double alpha, double beta) : GammaMembershipFunction(alpha, beta) {}
 
-class PiMembershipFunction : public MembershipFunction {
-private:
-    double alpha, beta, gamma, delta;
-public:
-    PiMembershipFunction(double alpha, double beta, double gamma, double delta) : alpha(alpha), beta(beta),
-                                                                                  gamma(gamma), delta(delta) {
-        // CHECK alpha < beta < gamma < delta
-    }
+        double calculateValue(const double &x) {
+            return 1 - GammaMembershipFunction::calculateValue(x);
+        }
+    };
 
-    ~PiMembershipFunction() {}
+    /*
+     * Similar to the lambda function, except there is no one value which is the center, there is an continuos interval
+     * where the center is.
+     */
+    class PiMembershipFunction : public MembershipFunction {
+    private:
+        double alpha, beta, gamma, delta;
+    public:
+        PiMembershipFunction(double alpha, double beta, double gamma, double delta) : alpha(alpha), beta(beta),
+                                                                                      gamma(gamma), delta(delta) {
+            // CHECK alpha < beta < gamma < delta
+        }
 
-    double calculateValue(const double &x) {
-        if (x < alpha || x >= delta) return 0;
-        else if (x < gamma && x >=beta) return 1;
-        else if (x < beta && x >= alpha) return (x - alpha) / (beta - alpha);
-        else return (delta - x) / (delta - gamma);
-    }
-};
+        ~PiMembershipFunction() {}
+
+        double calculateValue(const double &x) {
+            if (x < alpha || x >= delta) return 0;
+            else if (x < gamma && x >= beta) return 1;
+            else if (x < beta && x >= alpha) return (x - alpha) / (beta - alpha);
+            else return (delta - x) / (delta - gamma);
+        }
+    };
+}
+
+namespace SmoothMembershipFunction {
+
+    /*
+     * Smooth version of the gamma membership function.
+     * Beta should be (alpha + gamma) / 2.
+     */
+    class SMembershipFunction : public MembershipFunction {
+    private:
+        double alpha, beta, gamma;
+    public:
+        SMembershipFunction(double alpha, double beta, double gamma) : alpha{alpha}, beta{beta}, gamma{gamma} {
+            // CHECK alpha < beta < gamma
+        }
+
+        ~SMembershipFunction() {}
+
+        double calculateValue(const double &x) {
+            if (x < alpha) return 0;
+            else if (x >= gamma) return 1;
+            else if (x < beta && x >= alpha) {
+                double f = (x - alpha) / (gamma - alpha);
+                return 2 * f * f;
+            } else {
+                double f = (x - gamma) / (gamma - alpha);
+                return 1 - 2 * f * f;
+            }
+        }
+    };
+
+    /*
+     * Gauss membership function. mu is the mean, sigma std deviation.
+     */
+    class GaussMembershipFunction : public MembershipFunction {
+    private:
+        double mu, sigma;
+    public:
+        GaussMembershipFunction(double mu, double sigma) : mu(mu), sigma(sigma) {}
+
+        ~GaussMembershipFunction() {}
+
+        double calculateValue(const double &x) {
+            double exponent = (x - mu) / sigma;
+            return exp(-exponent * exponent);
+        }
+    };
+
+    /*
+     * Sigmoidal membership function. c decides where 0.5 value is, a how steep the climb is.
+     */
+    class SigmoidMembershipFunction : public MembershipFunction {
+    private:
+        double a, c;
+    public:
+        SigmoidMembershipFunction(double a, double c) : a(a), c(c) {}
+
+        ~SigmoidMembershipFunction() {}
+
+        double calculateValue(const double &x) {
+            return 1 / (1 + exp(-a * (x - c)));
+        }
+    };
+
+    /*
+     * Pi membership function. gamma represents the center, beta the bell width.
+     */
+    class PiMembershipFunction : public MembershipFunction {
+    private:
+        double beta, gamma;
+    public:
+        PiMembershipFunction(double beta, double gamma) : beta(beta), gamma(gamma) {}
+
+        ~PiMembershipFunction() {}
+
+        double calculateValue(const double &x) {
+            MembershipFunction *f = nullptr;
+            if (x < gamma) {
+                f = new SMembershipFunction(gamma - beta, gamma - beta / 2, gamma);
+                return f->calculateValue(x);
+            } else {
+                f = new SMembershipFunction(gamma, gamma + beta / 2, gamma + beta);
+                return 1 - f->calculateValue(x);
+            }
+
+        }
+    };
+
+    /*
+     * Exponentiallike membership function. mu represents the center, k the bell width.
+     * This function is nowhere equal to 0.
+     */
+    class ExponentialLikeMembershipFunction : public MembershipFunction {
+    private:
+        double mu, k;
+    public:
+        ExponentialLikeMembershipFunction(double mu, double k) : mu(mu), k(k) {}
+
+        virtual ~ExponentialLikeMembershipFunction() {
+
+        }
+
+        double calculateValue(const double &x) {
+            return 1 / (1 + k * (x - mu) * (x - mu));
+        }
+    };
+
+}
