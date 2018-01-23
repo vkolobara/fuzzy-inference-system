c0aa79f1933224bac73d9a46471af2233993f722
Vinko Kolobara <kolobara.vinko@gmail.com>
2018-01-23 21:38:13 +0100

    simulator changed to work with build dir

--- simulator/run.sh
+++ simulator/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/home/vkolobara/git/fuzzy-inference-system/cmake-build-debug/fuzzy-inference-system/fuzzy-inference-system /home/vkolobara/git/fuzzy-inference-system/fuzzy-inference-system/democonf/lang_variables.conf /home/vkolobara/git/fuzzy-inference-system/fuzzy-inference-system/democonf/rules.conf 
+/home/vkolobara/git/fuzzy-inference-system/build/fuzzy-inference-system/fuzzy-inference-system /home/vkolobara/git/fuzzy-inference-system/fuzzy-inference-system/democonf/lang_variables.conf /home/vkolobara/git/fuzzy-inference-system/fuzzy-inference-system/democonf/rules.conf 
